b1f3a3cb5b7228210306ea70466b318657c09128
Aleksandr <GReddyd1990@gmail.com>
2021-06-11 13:02:35 +0300

    Add code class Calc

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 public class CalcTest {
 	private static int monthPay;
 	private static int overPay;
-	private static final Calc credit = new Calc();
 	private static final int sum = 500000;
 	private static final int months = 24;
 	private static final int rates = 12;
 
+	private static final Calc credit = new Calc();
+
 	@BeforeAll
-	public static void TestData() {
+	public static void createTestData() {
 		monthPay = credit.getMonthlyPay(sum, months, rates);
 		overPay = credit.getOverpayAmount(monthPay, months, sum);
 	}
